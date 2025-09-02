4c5ce2907dcde2e3dab08d6c7743f257c13f9886
Isaac Glenzinski <isaacglenzinski@gmail.com>
2025-09-01 20:27:13 -0500

    Adding Windows control app

---
+++ AppRelayControl/AppRelayControl/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.IO.Ports;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppRelayControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort? Port;
+        List<Button> ButtonList = [];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cboComPort.ItemsSource = SerialPort.GetPortNames();
+
+            for (int i = 1; i <= 16; i++)
+            {
+                StackPanel tmpStackPanel = new()
+                {
+                    Orientation = Orientation.Vertical,
+                    Margin = new Thickness(5)
+                };
+                Grid.SetColumn(tmpStackPanel, (i > 8) ? 0 : 1);
+                Grid.SetRow(tmpStackPanel, (i > 8) ? Math.Abs(9 - i) : Math.Abs(8 - i));
+
+                Label tmpLabel = new()
+                {
+                    Content = "Relay " + i.ToString(),
+                };
+
+                Button tmpButton = new()
+                {
+                    Content = "OFF",
+                    Tag = i.ToString(),
+                    Background = Brushes.LightGray,
+                    IsEnabled = false
+                };
+                tmpButton.Click += RelayButton_Click;
+                ButtonList.Add(tmpButton);
+
+                tmpStackPanel.Children.Add(tmpLabel);
+                tmpStackPanel.Children.Add(tmpButton);
+
+                ButtonGrid.Children.Add(tmpStackPanel);
+            }
+        }
+
+        private void btnStartCom_Click(object sender, RoutedEventArgs e)
+        {
+            if (cboComPort.SelectedIndex >= 0)
+            {
+                Port = new SerialPort(cboComPort.Text, 115200, Parity.None);
+                try
+                {
+                    Port.Open();
+                    foreach (Button Btn in ButtonList)
+                    {
+                        Btn.IsEnabled = true;
+                        Port?.Write([4], 0, 1);
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show("ERROR - " + Ex.Message);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Must select a COM port!");
+            }
+        }
+
+        private void RelayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button ClickedButton)
+            {
+                string? ButtonTag = ClickedButton.Tag.ToString();
+                byte? RelayNum = ButtonTag != null ? byte.Parse(ButtonTag) : null;
+                if (RelayNum != null) { Port?.Write([1, RelayNum.Value], 0, 2); }
+
+                string? Value = ClickedButton.Content.ToString();
+                if (Value != null)
+                {
+                    if (Value == "OFF")
+                    {
+                        ClickedButton.Content = "ON";
+                        ClickedButton.Background = Brushes.LightGreen;
+                    }
+                    else
+                    {
+                        ClickedButton.Content = "OFF";
+                        ClickedButton.Background = Brushes.LightGray;
+                    }
+                }
+            }
+        }
+
+        ~MainWindow()
+        {
+            Port?.Close();
+        }
+
+        private void cboComPort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(cboComPort.SelectedIndex >= 0)
+            {
+                btnStartCom.IsEnabled = true;
+            }
+        }
+    }
+}
